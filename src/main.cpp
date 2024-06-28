db1f783b7ba3d46ddf2460d4ab58a36093b61f4a
christianwolff1 <chrstn_wolff@hotmail.com>
2024-06-27 21:02:24 -0400

    First change in the new branch

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,7 @@
 int main() {
 	printf("CppSource version 0.1.\n");
 	printf("This version focuses only in single file compiling using all the GCC toolchain.\n");
+	printf("This line will be only be visible in the branch\n");
 
 } // end of 'main'
 
